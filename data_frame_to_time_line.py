5ef7c59795cb87e4382896c67476d2d06943d14c
Syun Lee <pysyun@vitche.com>
2024-07-19 19:50:49 +0300

    - in the Storage.Timeline format, converted time to the UNIX timestamp in milliseconds

--- data_frame_to_time_line.py
+++ data_frame_to_time_line.py
@@ -2,14 +2,16 @@ class DataFrameToTimeLine:
 
     def process(self, data):
         """
-        Convert the DataFrame into an array of JSON objects with 'time' and 'value' fields.
+        Convert the DataFrame into an array of JSON objects with 'time' in UNIX timestamp (milliseconds)
+        and 'value' fields.
 
         :return: List of JSON objects
         """
         results = []
         for index, row in data.iterrows():
+            unix_time_ms = int(row["time"].timestamp() * 1000)  # Convert time to UNIX timestamp in milliseconds
             json_obj = {
-                "time": row["time"],
+                "time": unix_time_ms,
                 "value": {
                     "open": row["open"],
                     "high": row["high"],
