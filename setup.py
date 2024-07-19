0911c04454bc0877992bd22155fca67872fe5e01
Syun Lee <pysyun@vitche.com>
2024-07-19 19:52:48 +0300

    - added the library definition

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pysyun_binance_source',
+    version='1.0.1',
+    author='Syun Lee',
+    author_email='pysyun@vitche.com',
+    py_modules=['klines_source', 'data_frame_to_time_line']
+)
